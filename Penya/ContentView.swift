176fcfce66d7929bc27614536c46e8ec0051520e
Zach Coriarty <zackcor27@gmail.com>
2022-01-21 14:48:05 -0500

    Initial Commit

---
+++ Penya/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Penya
+//
+//  Created by Zachary Coriarty on 1/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Penya/PenyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenyaApp.swift
+//  Penya
+//
+//  Created by Zachary Coriarty on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct PenyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
